ebef2c02a6e6cd043a49ac1f48a00f2f9f70f4db
saladiner-jason <saladiner.jason@tamu.edu>
2022-09-20 18:48:19 -0500

    project class setup

---
+++ Project1/AERO472.py
@@ -0,0 +1,14 @@
+"""
+Names: Jason Saladiner Jr.
+Project: First projected of AERO 472- Fall of 2022
+Design of a wing and tail for minimizing fuel cost
+
+2 phase mission:
+    1) 300nm @ 150 kts (true airspeed) cruise
+    2) 2 hour loiter
+Mission occurs at 12,000ft 
+"""
+
+
+import AERO472 as p
+p.test()
---
+++ Project1/AERO472/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package will consist of given variables and general functions
+"""
+
+def test():
+    print("test")
